b840f35555fd87153958ecc0dce8a8331e85c612
Sebastian Pfohl <sebastian@pfohlnet.de>
2015-03-18 08:35:01 +0100

    Fixed Bug in ListView Control
    Written Converter to Collapse a TextBlock if the Binding String is empty
    or null. The App should now have the same look as the WP7 Version.

--- KeePass/Controls/ListItemView.xaml.cs
+++ KeePass/Controls/ListItemView.xaml.cs
@@ -1,5 +1,13 @@
 ﻿using System;
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Threading.Tasks;
+
 namespace KeePass.Controls
 {
     public partial class ListItemView
---
+++ KeePass/Utils/LengthConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Threading.Tasks;
+
+namespace KeePass.Utils
+{
+    public class LengthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string text = (string)value;
+            return text.Length > 0 ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
