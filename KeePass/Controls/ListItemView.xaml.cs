da97dab1e8c393eb87cce409098f1c944847aa28
Sebastian Pfohl <sebastian@pfohlnet.de>
2015-03-18 08:37:54 +0100

    Deleted unnessary Code

--- KeePass/Controls/ListItemView.xaml.cs
+++ KeePass/Controls/ListItemView.xaml.cs
@@ -1,13 +1,5 @@
 ﻿using System;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Data;
-using System.Threading.Tasks;
-
 namespace KeePass.Controls
 {
     public partial class ListItemView
